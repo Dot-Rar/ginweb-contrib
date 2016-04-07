5ba4b8eb25d05eda6d200431df4e1e7b695b839e
ZHU HAIHUA <kimiazhu@gmail.com>
2016-04-07 22:33:40 +0800

    add tests

--- sessions/mongo_test.go
+++ sessions/mongo_test.go
@@ -4,15 +4,35 @@
 package sessions
 
 import (
-	ginsession "github.com/gin-gonic/contrib/sessions"
+	. "github.com/gin-gonic/contrib/sessions"
 	"gopkg.in/mgo.v2"
 	"testing"
 )
 
 const mongoTestServer = "localhost:6379"
 
-var newRedisStore = func(_ *testing.T) ginsession.Store {
+var newMongoStore = func(_ *testing.T) Store {
 	dbsess, err := mgo.Dial(mongoTestServer)
 	store := NewMongoStore(dbsess, "session", "session", 3600, true, []byte("secret"))
 	return store
+}
+
+func TestMongo_SessionGetSet(t *testing.T) {
+	sessionGetSet(t, newMongoStore)
+}
+
+func TestMongo_SessionDeleteKey(t *testing.T) {
+	sessionDeleteKey(t, newMongoStore)
+}
+
+func TestMongo_SessionFlashes(t *testing.T) {
+	sessionFlashes(t, newMongoStore)
+}
+
+func TestMongo_SessionClear(t *testing.T) {
+	sessionClear(t, newMongoStore)
+}
+
+func TestMongo_SessionOptions(t *testing.T) {
+	sessionOptions(t, newMongoStore)
 }
